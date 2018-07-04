159a894a20e3938870c2898b8279f2f50eee9039
Xuefeng <Xuefeng.Chang@thomsonreuters.com>
2018-07-04 04:45:27 -0400

    switch to qin

--- Civ6Cheat/Civil6Cheat.sql
+++ Civ6Cheat/Civil6Cheat.sql
@@ -1,14 +1,15 @@
-UPDATE AiFavoredItems SET Value='-100' WHERE Item='PSEUDOYIELD_NUCLEAR_WEAPON';
+UPDATE AiFavoredItems SET Value='0', Favored='0'  WHERE Item='PSEUDOYIELD_NUCLEAR_WEAPON';
 
 INSERT INTO Types ('Type', 'Kind') VALUES ('TRAIT_HUMAN_PLAYER', 'KIND_TRAIT');
 INSERT INTO Types ('Type', 'Kind') VALUES ('BUILDING_HUMAN_PLAYER', 'KIND_BUILDING');
 INSERT INTO Traits ('TraitType', 'Name', 'Description') VALUES ('TRAIT_HUMAN_PLAYER', 'AAA Trait', 'AAA Cheat trait for human player');
 INSERT INTO CivilizationTraits ('CivilizationType', 'TraitType') VALUES ('CIVILIZATION_CHINA', 'TRAIT_HUMAN_PLAYER');
 
-INSERT INTO Buildings ('BuildingType', 'TraitType', 'Name', 'Description', 'PurchaseYield', 'Cost', 'AdvisorType', PrereqTech, PrereqDistrict) VALUES ('BUILDING_HUMAN_PLAYER', 'TRAIT_HUMAN_PLAYER', 'AAA Building', 'AAA  Cheating Building for Human Player', 'YIELD_GOLD', '1', 'ADVISOR_CULTURE', NULL, 'DISTRICT_CITY_CENTER');
+INSERT INTO Buildings ('BuildingType', 'TraitType', 'Name', 'Description', 'PurchaseYield', 'Cost', 'AdvisorType', PrereqTech, PrereqDistrict, Housing) VALUES ('BUILDING_HUMAN_PLAYER', 'TRAIT_HUMAN_PLAYER', 'AAA Building', 'AAA  Cheating Building for Human Player', 'YIELD_GOLD', '1', 'ADVISOR_CULTURE', NULL, 'DISTRICT_CITY_CENTER', '10');
 
 
-UPDATE Units SET BaseMoves='3', Range="4" WHERE UnitType='UNIT_ROCKET_ARTILLERY';
+UPDATE Units SET BaseMoves='3', Range='4' WHERE UnitType='UNIT_ROCKET_ARTILLERY';
+UPDATE Units SET BaseSightRange='3', AirSlots='5' WHERE UnitType='UNIT_AIRCRAFT_CARRIER';
 
 -- UPDATE Units SET PrereqTech=NULL WHERE UnitType='UNIT_BOMBER';
 -- UPDATE Units SET PrereqTech=NULL WHERE UnitType='UNIT_FIGHTER';
@@ -24,3 +25,9 @@ INSERT INTO Unit_BuildingPrereqs ('UNIT', 'PrereqBuilding') VALUES ('UNIT_MODERN
 INSERT INTO Unit_BuildingPrereqs ('UNIT', 'PrereqBuilding') VALUES ('UNIT_NUCLEAR_SUBMARINE', 'BUILDING_HUMAN_PLAYER');
 INSERT INTO Unit_BuildingPrereqs ('UNIT', 'PrereqBuilding') VALUES ('UNIT_AIRCRAFT_CARRIER', 'BUILDING_HUMAN_PLAYER');
 INSERT INTO Unit_BuildingPrereqs ('UNIT', 'PrereqBuilding') VALUES ('UNIT_MISSILE_CRUISER', 'BUILDING_HUMAN_PLAYER');
+
+-- UPDATE Buildings SET TraitType='TRAIT_HUMAN_PLAYER', RequiresPlacement='0' WHERE IsWonder='1';
+-- UPDATE Buildings SET ObsoleteEra='NO_ERA', MaxWorldInstances = '-1', MaxPlayerInstances = '1' WHERE IsWonder='1';
+UPDATE Buildings SET ObsoleteEra='NO_ERA', TraitType='TRAIT_HUMAN_PLAYER' WHERE IsWonder='1';
+
+INSERT INTO Building_YieldChanges('BuildingType', 'YieldType', 'YieldChange') VALUES ('BUILDING_HUMAN_PLAYER', 'YIELD_PRODUCTION', '20');
